8ed7f8cfc36c09050dd44d12f40170aef68d4375
peet1126 <pbmcdonald7@gmail.com>
2021-08-19 12:13:49 -0500

    Test Commit

--- src/Components/CalculatorDetails.jsx
+++ src/Components/CalculatorDetails.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import LoanCollateral from './LoanCollateral';
 
 class CalculatorDetails extends React.Component {
-    render () {
+    render() {
 
-        const {amount, months, ltv, repayment} = this.props;
+        const { amount, months, ltv, repayment } = this.props;
 
-        // Calculations --
+        // Calculations 
         const apr = (Number(ltv) / 10) + 4;
 
         const monthlyInterestRate = (apr / 100) / 12;
-    
-        let monthlyPayment = (Number(amount) * monthlyInterestRate * (1 + monthlyInterestRate)** months) / ((1 + monthlyInterestRate)** months - 1);
+
+        let monthlyPayment = (Number(amount) * monthlyInterestRate * (1 + monthlyInterestRate) ** months) / ((1 + monthlyInterestRate) ** months - 1);
 
         let totalInterest = ((monthlyPayment * months) - Number(amount));
-        
+
         let totalLoanCost = Number(amount) + totalInterest;
 
         let interestOnly = totalInterest / months;
@@ -26,7 +26,7 @@ class CalculatorDetails extends React.Component {
             let num = parseInt(number);
             num = Math.round(100 * number) / 100;
             return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-        } 
+        }
 
         // ToDo - Write this in a cleaner way 
         const principal = numberFormat(amount);
@@ -40,12 +40,12 @@ class CalculatorDetails extends React.Component {
 
 
 
-        
+
         // Interest Only or Principal & Interest conditional rendering
-        let payments = (repayment === "Interest Only" ? 
+        let payments = (repayment === "Interest Only" ?
             <div>
                 <span className="monthly-payment-amount">${interestOnly}</span>
-                <h4 id="last-payment">Last Payment: ${lastPayment}</h4> 
+                <h4 id="last-payment">Last Payment: ${lastPayment}</h4>
             </div>
             :
             <span className="monthly-payment-amount">${monthlyPayment}</span>
@@ -54,33 +54,33 @@ class CalculatorDetails extends React.Component {
         return (
             <div className="calculator-details">
                 <div className="calculator-layout">
-                <div className="details-payment">
-                    <h4 className="monthly-payment-label">Monthly Payment ({months} months)</h4>
-                    {payments}
-                </div>
-                <div className="loan-stats-row">
-                    <div className="loan-stats">
-                        <h4>Loan Amount</h4>
-                        <span>${principal}</span>
+                    <div className="details-payment">
+                        <h4 className="monthly-payment-label">Monthly Payment ({months} months)</h4>
+                        {payments}
                     </div>
-                    <div className="loan-stats">
-                        <h4>APR</h4>
-                        <span>{apr}%</span>
+                    <div className="loan-stats-row">
+                        <div className="loan-stats">
+                            <h4>Loan Amount</h4>
+                            <span>${principal}</span>
+                        </div>
+                        <div className="loan-stats">
+                            <h4>APR</h4>
+                            <span>{apr}%</span>
+                        </div>
                     </div>
-                </div>
-                <div className="loan-stats-row">
-                    <div className="loan-stats">
-                        <h4>Total Loan Cost</h4>
-                        <span>${totalLoanCost}</span>
+                    <div className="loan-stats-row">
+                        <div className="loan-stats">
+                            <h4>Total Loan Cost</h4>
+                            <span>${totalLoanCost}</span>
+                        </div>
+                        <div className="loan-stats">
+                            <h4>Interest</h4>
+                            <span>${totalInterest}</span>
+                        </div>
                     </div>
-                    <div className="loan-stats">
-                        <h4>Interest</h4>
-                        <span>${totalInterest}</span>
-                    </div>
+                    <LoanCollateral amount={amount} ltv={ltv} numberFormat={numberFormat} />
                 </div>
-                <LoanCollateral amount={amount} ltv={ltv} numberFormat={numberFormat} />
-                </div>
-                
+
             </div>
         )
     }
